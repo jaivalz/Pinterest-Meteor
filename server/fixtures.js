0b81c6d5428f28bf551e17f8806ca5be71a86af1
Jaime <jaivalz@yahoo.com>
2016-03-06 23:30:56 +0100

    Created app, installed bootstrap, added structure, created layout template, created post collection, added static post data

---
+++ server/fixtures.js
@@ -0,0 +1,43 @@
+if (Posts.find().count()=== 0) {
+  Posts.insert({
+    title: 'Primer Post',
+    description: 'Este es el primer post, espero que vaya guay',
+    media_url: 'http://vignette2.wikia.nocookie.net/arresteddevelopment/images/2/26/S4_Transparent_Tobias.png/revision/latest?cb=20130502031926',
+    url: 'https://www.jaiv.me'
+  });
+
+  Posts.insert({
+    title: 'Segundo Post',
+    description: 'Minuto 2.21 chicos',
+    media_url: 'http://st.forocoches.com/foro/customavatars/avatar654567_33.gif',
+    url: 'https://www.jaiv.me'
+  });
+
+  Posts.insert({
+    title: 'Tercer Post',
+    description: 'He estado más cerca de polear que ese pezón de asomar',
+    media_url: 'http://st.forocoches.com/foro/customavatars/avatar316969_2.gif',
+    url: 'https://www.jaiv.me'
+  });
+
+  Posts.insert({
+    title: 'Cuarto Post',
+    description: 'Joder, Guillermo, tiene que tener las bolas de acero para estar ahí aguantando ese beso esquimal. Como está la Charlize',
+    media_url: 'http://vignette2.wikia.nocookie.net/arresteddevelopment/images/2/26/S4_Transparent_Tobias.png/revision/latest?cb=20130502031926',
+    url: 'https://www.jaiv.me'
+  });
+
+  Posts.insert({
+    title: 'Quinto Post',
+    description: 'Son boers sanos. Lekker lekker',
+    media_url: 'http://st.forocoches.com/foro/customavatars/avatar537100_78.gif',
+    url: 'https://www.jaiv.me'
+  });
+
+  Posts.insert({
+    title: 'Sexto Post',
+    description: 'que tío más grande no lo conocía, me he rajado cuando llama loser a Matt Damon',
+    media_url: 'http://www.blogodisea.com/wp-content/uploads/2013/11/charlize-theron-sin-maquillaje.jpg',
+    url: 'https://www.jaiv.me'
+  });
+}

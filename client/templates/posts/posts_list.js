ebb6f95960dac66688ee67b2e6e4cc9b32f3400d
Jaime <jaivalz@yahoo.com>
2016-03-07 15:54:42 +0100

    Added navbar

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -2,4 +2,16 @@ Template.postsList.helpers({
   posts: function() {
     return Posts.find();
   }
-})
+});
+
+Template.postsList.rendered = function() {
+  $('.grid').imagesLoaded(function(){
+      // images have loaded
+      // Use the packery jquery plugin
+      $('.grid').packery({
+        //options
+        itemSelector: '.item',
+        gutter: 10
+      });
+  });
+};
